e9057df6d4496a5379b7a32cd3dd143974db4e63
mosafari <m.safari2680@gmail.com>
2022-12-28 18:58:09 +0330

    add fib function

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,11 +1,19 @@
 # importing modules
-
-
-
-
+import logging
+import threading
 
 # fibunacci func
+def fib(n):
+    """Calculate the Nth fibonacci number"""
+    if n < 0:
+        raise ValueError('Negative numbers are not supported')
+    elif n == 0:
+        return 0
+    elif n <= 2:
+        return 1
 
+    return fib(n - 2) + fib(n - 1)
+    
 
 
 # main 
